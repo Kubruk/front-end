b8c053ccf4fbed536a5968d1b2797cbc2134f722
Kubruk <99119267+Kubruk@users.noreply.github.com>
2022-06-26 16:13:31 +0200

    added call to retrieve books

---
+++ src/components/BookCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white rounded-lg p-8 shadow-sm m-4">
+    <h2 class="text-3xl pb-4">{{ title }}</h2>
+    <div class="flex">
+      <img :src="portrait" :alt="title" class="h-96 w-60" />
+      <div class="pl-4 text-ellipsis overflow-hidden w-64">
+        {{ description }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    portrait: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/modules/home/HomePage.vue
+++ src/modules/home/HomePage.vue
@@ -1,10 +1,27 @@
 <template>
   <main class="main-page flex items-center justify-center">
-    <h1 class="home">Home</h1>
+    <BookCard
+      v-for="book in books"
+      :key="book.title"
+      :title="book.title"
+      :description="book.description"
+      :portrait="book.portrait"
+    />
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import BookCard from '@components/BookCard.vue';
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const books = ref([]);
+onMounted(() => {
+  axios
+    .get('https://kubruk-back.herokuapp.com/api/books')
+    .then(({ data }) => (books.value = data.books));
+});
+</script>
 
 <style lang="scss" scoped>
 @use '@styles/home/home.module.scss';
