5ef9ed1a6b042a5d49dfd787d85d246e4a65025f
Kubruk <99119267+Kubruk@users.noreply.github.com>
2022-06-27 23:58:25 +0200

    styling

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,15 +1,19 @@
 <template>
   <div
-    class="bg-white border rounded-lg shadow-sm mb-4 md:mb-0 place-self-stretch"
+    class="bg-white border rounded-lg shadow-sm mb-4 md:mb-0 place-self-center w-full"
   >
     <h2
       class="text-3xl p-4 bg-bright-gray text-alabaster-400 text-ellipsis break-all whitespace-nowrap overflow-hidden"
     >
       {{ title }}
     </h2>
-    <div class="flex">
-      <img :src="cover" :alt="title" class="w-1/5 h-1/2 md:w-1/3" />
-      <div class="pl-4 h-1/2">
+    <div class="flex h-60">
+      <img
+        :src="cover"
+        :alt="title"
+        class="object-cover w-1/5 md:w-1/3 h-full"
+      />
+      <div class="p-4 h-full overflow-hidden">
         {{ description }}
       </div>
     </div>
