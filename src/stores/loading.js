1e423218bd854ebd128d417494cc7faf8e68c5e3
Kubruk <99119267+Kubruk@users.noreply.github.com>
2022-07-11 20:12:48 +0200

    removed console logs

--- src/stores/loading.js
+++ src/stores/loading.js
@@ -12,9 +12,7 @@ export const loadingStore = defineStore('loading', {
       this.loading.push(loadingStatus);
     },
     unsetLoading(loadingStatus) {
-      console.log(this.loading, this.loading.indexOf(loadingStatus));
       this.loading.splice(this.loading.indexOf(loadingStatus), 1);
-      console.log(this.loading);
     }
   }
 });
